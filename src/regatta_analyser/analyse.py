bd6c3bf854567548d1c8e88830ec7eba0397579c
siimtolk <20063426+siimtolk@users.noreply.github.com>
2023-11-18 22:38:30 +0200

    move from flat layout to src layout

--- src/regatta_analyser/analyse.py
+++ src/regatta_analyser/analyse.py
@@ -1,10 +1,9 @@
 from . import *
-from utils import *
+from .utils import *
 import duckdb 
 
 
 def analyse(regatta_log_path):
-
     create_file_if_not_there(TMP_DB_FILE_PATH)
-    print(f' Analyse logs in {regatta_log_path}')
-
+    print('asd')
+    #print(f'Analyse logs in {regatta_log_path}')
--- src/regatta_analyser/utils.py
+++ src/regatta_analyser/utils.py
@@ -1,6 +1,7 @@
 import os
 
 def create_file_if_not_there(file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
     if not os.path.exists(file_path):
         with open(file_path, 'w'):
             pass  # Creates an empty file
