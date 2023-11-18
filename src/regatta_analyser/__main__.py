733924c68f6aab306edeeb8627a8770b7c2d2d6b
siimtolk <20063426+siimtolk@users.noreply.github.com>
2023-11-18 23:57:41 +0200

    Analyser class, import and transform csv with sql

--- src/regatta_analyser/__main__.py
+++ src/regatta_analyser/__main__.py
@@ -1,18 +1,19 @@
 # __main__.py
-from .analyse import analyse
+from .analyse import Analyser
 import sys
 
 def main():
     # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 2:
-        print("Usage: python -m regatta_analyser <file_path>")
+        print("Usage: regatta_analyser <regatta log path.csv>")
         sys.exit(1)
 
     # Get the file path from the command-line arguments
-    file_path = sys.argv[1]
+    log_file_path = sys.argv[1]
 
     # Call the analyse function with the provided file path
-    analyse(file_path)
+    a = Analyser(log_file_path)
+    
 
 if __name__ == "__main__":
     main()
--- src/regatta_analyser/analyse.py
+++ src/regatta_analyser/analyse.py
@@ -3,7 +3,49 @@ from .utils import *
 import duckdb 
 
 
-def analyse(regatta_log_path):
-    create_file_if_not_there(TMP_DB_FILE_PATH)
-    print('asd')
-    #print(f'Analyse logs in {regatta_log_path}')
+class Analyser():
+
+    def __init__(self, log_file):
+        # Connect to the Duck DB database
+        self.duck = duckdb.connect()
+        self.log_file = log_file
+        self.dict_tbls = {} #list of all duck db table references
+
+        print(f'Import logs from {self.log_file}')
+        self.create_tbl_raw_data()
+        self.print_duckdb_tables()
+        print('Creating agg_data table...')
+        self.create_tbl_avg()
+        print('Printing results from agg_data table...')
+        self.print_duckdb_tables()
+
+        self.print_table('agg_data')
+
+    # ------------------------------------------------------------- #
+
+    def print_duckdb_tables(self):
+        print(self.dict_tbls)
+
+    def create_tbl_raw_data(self):
+        tbl_name = 'tbl_raw_data'
+        sql_query = f"CREATE OR REPLACE TABLE {tbl_name} AS SELECT * FROM read_csv_auto('{self.log_file}')"
+        self.duck.execute(sql_query)
+        self.dict_tbls['raw_data'] = tbl_name
+
+    def create_tbl_avg(self):
+        tbl_name = 'tbl_agg_data'
+        sql_query = f''' CREATE OR REPLACE TABLE {tbl_name} AS 
+                    SELECT avg(sog) as avg_sog
+                          , avg(tws) as avg_tws
+                          , COUNT(*) AS count 
+                    FROM {self.dict_tbls['raw_data']}
+                    '''
+        self.duck.execute(sql_query)
+        self.dict_tbls['agg_data'] = tbl_name
+
+
+    def print_table(self, tbl_name):
+
+        result = self.duck.execute(f'SELECT * FROM {self.dict_tbls[tbl_name]}').fetchall()
+        print(f' Table {tbl_name} -->> {result})')
+
